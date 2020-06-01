dfd92161476ea77c486c40162d3e7666d4a6ee25
Cece Claridge <cece.claridge@gmail.com>
2020-06-01 13:02:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// JUST FOR DEMO PURPOSES, PUT YOUR ACTUAL API CODE HERE
-app.get('/api/demo', (request, response) => {
-  response.json({
-    message: 'Hello from server.js'
-  });
-});
-// END DEMO
-
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
